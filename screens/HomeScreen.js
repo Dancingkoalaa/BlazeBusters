9dec02d3cefbc68ea65fa41fd72f14de6729c094
Tim.Wetzels <tim.wetzels@motion10.com>
2023-06-02 10:43:15 +0200

    added call functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking, Button } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Welcome to the Home Screen!</Text>
+      <Button onPress={()=>{Linking.openURL(`tel:${+310682371526}`);}} title='Bel Tim'></Button>
     </View>
   );
 }
+//
 
 const styles = StyleSheet.create({
   container: {
